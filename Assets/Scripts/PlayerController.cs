2ab5e22f1ee24ae780ade7e17ab445ee99d448b4
Twitchton <Twitchton@gmail.com>
2023-09-13 15:44:44 -0300

    Added Loading background
    Added Loading background, still needs to be projected onto a panel.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,23 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public float timer = 0;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+
     private Rigidbody rb;
     private float movementX, movementY;
-    public float speed=0;
+    private int count;
 
     // Start is called before the first frame update
-    void Start(){
-
+    void Start()
+    { 
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetScoreText();
     }
 
     // Update is called once per frame
     void Update(){
 
-        
+        timer -= (Time.deltaTime);
+
+        SetScoreText();
+        SetTimerText();
+    }
+
+    //Function that updates score text on the GUI when called
+    void SetScoreText()
+    {
+        scoreText.text = "Score:" + count.ToString();
+    }
+
+    //Function that updates the timer text on the GUI when called
+    void SetTimerText()
+    {
+        timerText.text = "Timer\n" + timer.ToString();
     }
 
     //Function called when player movement is called
@@ -38,5 +62,15 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement*speed);
     }
 
+    //calls function when colliding with a trigger 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+        }
+       
+    }
 
 }

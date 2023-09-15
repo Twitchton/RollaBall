5b9e422cf12a0f41216833fa75ebffbcab701caa
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2023-09-15 13:22:38 -0300

    Completed other major features
    Added Pause Menu and End Menu to the game and keybinds for them using the new unity input manager. Other features to potentially build in would be a fixed score calculator based on time, BG music and sounds, randomized starting positions for collectibles, and a rolling score counter.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //keybinds
     [SerializeField]
     private InputActionReference start, pause, restart;
+
     //Text
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI endtext;
+    public TextMeshProUGUI finalScore;
 
     //UI
     public GameObject titleScreen;
@@ -21,11 +26,27 @@ public class GameController : MonoBehaviour
 
     //tracked values
     public float timer = 0;
-    private int score;
+    [SerializeField] private float timeMod = 0;
     public int collectibles = 0;
+    private int score, collected;
+    
 
     //state
     private string state;
+
+    private void OnEnable()
+    {
+        start.action.performed += startGame;
+        pause.action.performed += pauseGame;
+        restart.action.performed += restartGame;
+    }
+
+    private void OnDisable()
+    {
+        start.action.performed -= startGame;
+        pause.action.performed -= pauseGame;
+        restart.action.performed -= restartGame;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -42,25 +63,102 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (state == "start")
+        if (state == "play")
         {
-            
-        }
-        timer -= (Time.deltaTime);
+            timer -= (Time.deltaTime);
 
-        SetScoreText();
-        SetTimerText();
+            SetScoreText();
+            SetTimerText();
+        }
+
+        if (collected >= collectibles)
+        {
+            state = "finish";
+
+            SetEndText(true);
+            finishGame();
+        }
+
+        if (timer <= 0)
+        {
+            state = "finish";
+
+            SetEndText(false);
+            finishGame();
+        }
+        
     }
 
-    //Handles use of the submit key
-    void onSubmit()
+    //Function for starting the game from start menu
+    private void startGame(InputAction.CallbackContext context)
     {
         if (state == "start")
         {
+            state = "play";
+
+            collected = 0;
+
+            SetScoreText();
+            SetTimerText();
+
             titleScreen.SetActive(false);
             GUI.SetActive(true);
         }
+    }
 
+    //Function that pauses the game
+    private void pauseGame(InputAction.CallbackContext context)
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        if (state == "play")
+        {
+            state = "pause";
+            Time.timeScale = 0;
+
+            GUI.SetActive(false);
+            pauseScreen.SetActive(true);
+        }
+        else if (state == "pause")
+        {
+            state = "play";
+            Time.timeScale = 1;
+
+            GUI.SetActive(true);
+            pauseScreen.SetActive(false);
+        }
+    }
+
+    //function that restarts the game
+    private void restartGame(InputAction.CallbackContext context)
+    {
+        if (state != "start")
+        {
+            Restart();
+        }
+    }
+
+    public void Restart()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //Function that ends the game and diplsays player score
+    void finishGame()
+    {
+        //score = (int)(score*timer/timeMod);
+        finalScore.text = "Final Score: " + score.ToString();
+
+        GUI.SetActive(false);
+        endScreen.SetActive(true);
     }
 
     //Function that updates score text on the GUI when called
@@ -76,43 +174,28 @@ public class GameController : MonoBehaviour
     }
 
     //Function that sets the end text 
-    void SetEndText()
+    void SetEndText(bool win)
     {
-
+        if (win)
+        {
+            endtext.text = "You WIN!";
+        }
+        else
+        {
+            endtext.text = "You Lose... :(";
+        }
     }
 
     //Function that increases the score
     public void getPickup()
     {
-        score++;
+        collected++;
+        score = collected * 100;
     }
 
-    //initiates the game state
-    void startGame()
+    //Function that exits the game
+    public void Quit()
     {
-        score = 0;
-
-        SetScoreText();
-        SetTimerText();
-
-
+        Application.Quit();
     }
-
-    //Function that pauses the game
-    void pauseGame()
-    {
-
-    }
-
-    //Function that returns to game state from pause state
-    void continueGame()
-    {
-
-    }
-
-    void finishGame()
-    {
-
-    }
-
 }
